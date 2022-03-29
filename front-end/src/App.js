27f39474e51c2fdc280ca86c1dd181e2d63d9f88
shaiban <sl01347@surrey.ac.uk>
2022-03-29 02:01:25 +0100

    implemented blocking user from going to other routes when logged in

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,35 +3,42 @@ import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
 import Homepage from "./components/Homepage/Homepage";
 import LoginProvider from "./components/Login/LoginProvider";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import { useEffect, useContext } from "react";
 
 function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const loggedInUser = localStorage.getItem("user");
+    if (loggedInUser) {
+      return navigate("/homepage");
+    }
+  }, []);
   return (
-    <Router>
-      <LoginProvider>
-        <NavBar />
-        <div className="content">
-          <Routes>
-            {/* TODO: should "/" path lead to the login screen?
+    <LoginProvider>
+      <NavBar />
+      <div className="content">
+        <Routes>
+          {/* TODO: should "/" path lead to the login screen?
             if so then is there a need for "/login"? should it be
             changed to just "/"? */}
-            <Route path="/" element={<Login />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
+          <Route exact path="/" element={<Login />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
 
-            {/* authenticated links */}
-            <Route
-              path="/homepage"
-              element={
-                // <RequireAuth>
-                <Homepage />
-                // </RequireAuth>
-              }
-            />
-          </Routes>
-        </div>
-      </LoginProvider>
-    </Router>
+          {/* authenticated links */}
+          <Route
+            path="/homepage"
+            element={
+              // <RequireAuth>
+              <Homepage />
+              // </RequireAuth>
+            }
+          />
+        </Routes>
+      </div>
+    </LoginProvider>
   );
 }
 
