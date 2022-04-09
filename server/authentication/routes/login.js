2bf698fcd5ed346a698ab55f924794ad9e313c63
talhaahmedd <talhaa.ahmed1999@gmail.com>
2022-04-09 13:52:37 +0100

    Update login.js

--- server/authentication/routes/login.js
+++ server/authentication/routes/login.js
@@ -12,7 +12,7 @@ router.post('/', async (req, res) => {
 //    console.log(user)
    
     // Check email
-    if (!user) return res.status(400).send({message: 'Username or password is incorrect'})
+    if (!user) return res.status(200).send({message: 'Username or password is incorrect'})
 
     // Check Password
     const valid_password = await bcrypt.compare(req.body.password, user.password)
