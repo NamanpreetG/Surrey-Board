8a475ffed34b3f99e9378ee8991cd9e093fb5c7d
talhaahmedd <talhaa.ahmed1999@gmail.com>
2022-04-11 12:54:10 +0100

    completed retrieving comments for a certain post

--- server/posts/routes/comments.js
+++ server/posts/routes/comments.js
@@ -1,6 +1,67 @@
+const Post = require('../models/post')
+const Comments = require('../models/Comments')
+const router = require('express').Router()
+
+
+
+
 /**
- * add comments
- * 
- * show comments
+ * SHOW COMMENTS: based on post_id
  */
 
+router.get('/:post_id', async (req, res) => {
+    try {
+        const c = await Comments.
+            where('post_id').equals(req.params.post_id).
+            select('comment')
+
+        console.log(c);
+
+    } catch (error) {
+        console.log(error);
+
+    }
+
+});
+
+
+/**
+ * ADD COMMENT
+ */
+
+router.post('/add', (req, res) => {
+
+    console.log('ADD COMMENT');
+})
+
+//////////
+
+router.get('/z', async (req, res) => {
+
+    const comment1 = new Comments({
+        post_id: '625300b7c19ba3e830c521d0',
+        comment: 'THIS IS A COMMENT'
+    })
+    const comment2 = new Comments({
+        post_id: '625300b7c19ba3e830c521d0',
+        comment: 'THIS IS ANOTHER COMMENT'
+    })
+
+
+
+    const t = await Post.findOne({ _id: "625300b7c19ba3e830c521d0" }).populate('comments')
+    // t.comments = '62530972e64d1489370ae6e5'
+    // console.log(t)
+
+    Post.
+        findOne().
+        populate('comments').
+        exec(function (err, resultt) {
+            if (err) return handleError(err);
+            console.log('comments are: ', resultt.comments);
+        });
+
+    const t2 = await Post.find()
+    res.send(t2)
+});
+module.exports = router
--- server/posts/routes/showPost.js
+++ server/posts/routes/showPost.js
@@ -2,7 +2,7 @@ const Post = require('../models/post')
 const Comments = require('../models/Comments')
 const router = require('express').Router()
 
-router.get('/x', async (req, res) => {
+router.get('/', async (req, res) => {
     Post.find({}, (err, result) => {
         if (err) {
             res.send(err)
@@ -11,32 +11,4 @@ router.get('/x', async (req, res) => {
     })
 });
 
-router.get('/', async (req, res) => {
-
-    const comment1 = new Comments({
-        post_id: '625300b7c19ba3e830c521d0',
-        comment: 'THIS IS A COMMENT'
-    })
-    const comment2 = new Comments({
-        post_id: '625300b7c19ba3e830c521d0',
-        comment: 'THIS IS ANOTHER COMMENT'
-    })
-
-
-
-    const t = await Post.findOne({ _id: "625300b7c19ba3e830c521d0" }).populate('comments')
-    // t.comments = '62530972e64d1489370ae6e5'
-    // console.log(t)
-
-    Post.
-        find().
-        populate('comments').
-        exec(function (err, resultt) {
-            if (err) return handleError(err);
-            console.log('comments are: ', resultt.comments);
-        });
-
-    const t2 = await Post.find()
-    res.send(t2)
-});
 module.exports = router
