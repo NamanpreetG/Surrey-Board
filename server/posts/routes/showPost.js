747b87e17957e9037377d53409cdb0d40ac556ff
shaiban <sl01347@surrey.ac.uk>
2022-04-13 03:15:29 +0100

    save changes

--- server/posts/routes/showPost.js
+++ server/posts/routes/showPost.js
@@ -1,40 +1,42 @@
-const Post = require("../models/post");
-const Comments = require("../models/Comments");
-const router = require("express").Router();
-const mongoose = require("mongoose");
-const { response } = require("express");
+const Post = require('../models/post')
+const Comments = require('../models/Comments')
+const router = require('express').Router()
+const mongoose = require('mongoose')
+const { response } = require('express')
+
 
 const societySchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-});
+    name: {
+        type: String,
+        required: true
 
-const Society = mongoose.model("Society", societySchema);
+    }
+})
 
-router.get("/next", async (req, res) => {
-  var page_num = parseInt(req.query.page) + 1;
-  const count_val = parseInt(req.query.index);
+const Society = mongoose.model('Society', societySchema)
+
+
+router.get('/next', async (req, res) => {
+
+    var page_num = parseInt(req.query.page) + 1
+    const count_val = parseInt(req.query.index)
+
+    Post.find({ counter: { $lt: count_val } }).limit(10).sort('-counter').populate({
+
+        model: 'User',
+        path: 'user',
+        select: 'name isAdmin'
 
     }).populate('society').exec((err, result) => {
         if (err) {
             res.send({ message: 'error' })
         }
         else if (result.length === 0) {
+            
             page_num = null
         }
         res.send({ result: result, next: page_num })
     })
-    .populate("society")
-    .exec((err, result) => {
-      if (err) {
-        res.send({ message: "error" });
-      } else if (result.length === 0) {
-        page_num = null;
-      }
-      res.send({ result: result, next: page_num });
-    });
 });
 
 router.get('/previous', async (req, res) => {
@@ -42,7 +44,7 @@ router.get('/previous', async (req, res) => {
     const page_num = parseInt(req.query.page) - 1 ? parseInt(req.query.page) > 0 : null
     const count_val = parseInt(req.query.index)
 
-    Post.find({ counter: { $gt: count_val } }).limit(10).sort('date').populate({
+    Post.find({ counter: { $gt: count_val } }).limit(10).sort('counter').populate({
 
         model: 'User',
         path: 'user',
@@ -54,20 +56,13 @@ router.get('/previous', async (req, res) => {
         }
         res.send({ result: result.reverse(), next: page_num })
     })
-    .populate("society")
-    .exec((err, result) => {
-      if (err) {
-        res.send({ message: "error" });
-      }
-      res.send({ result: result.reverse(), next: page_num });
-    });
 });
 
 // General Board
 router.get('/', async (req, res) => {
 
     
-    Post.find({}).limit(10).sort('-date').populate({
+    Post.find({}).limit(10).sort('-counter').populate({
 
         model: 'User',
         path: 'user',
@@ -79,49 +74,42 @@ router.get('/', async (req, res) => {
         }
         res.send({ result: result, next: 2 })
     })
-    .populate("society")
-    .exec((err, result) => {
-      if (err) {
-        res.send({ message: "error" });
-      }
-      res.send({ result: result });
-    });
 });
 
 // Events Board
-router.get("/events", async (req, res) => {
-  Post.find({ isEvent: true })
-    .populate({
-      model: "User",
-      path: "user",
-      select: "name isAdmin",
+router.get('/events', async (req, res) => {
+    Post.find({ isEvent: true }).populate({
+
+        model: 'User',
+        path: 'user',
+        select: 'name isAdmin'
+
+    }).populate('society').sort('-date').exec((err, result) => {
+        if (err) {
+            res.send({ message: 'error' })
+        }
+        res.send(result)
     })
-    .populate("society")
-    .sort("-date")
-    .exec((err, result) => {
-      if (err) {
-        res.send({ message: "error" });
-      }
-      res.send(result);
-    });
 });
 
 // Society Board
-router.get("/society/:id", async (req, res) => {
-  Post.find({ society: req.params.id })
-    .sort("-date")
-    .populate({
-      model: "User",
-      path: "user",
-      select: "name isAdmin",
-    })
-    .populate("society")
-    .exec((err, result) => {
-      if (err) {
-        res.send({ message: "error" });
-      }
-      res.send(result);
-    });
+router.get('/society/:id', async (req, res) => {
+    Post.find({ society: req.params.id })
+        .sort('-date').populate({
+
+            model: 'User',
+            path: 'user',
+            select: 'name isAdmin'
+
+        })
+        .populate('society')
+        .exec((err, result) => {
+            if (err) {
+                res.send({ message: 'error' })
+            }
+            res.send(result)
+        })
+
 });
 
-module.exports = router;
+module.exports = router
