d18798ac3730ba04fed56ff8625f8f87d4cd9242
Armand2312 <armand.oral231@gmail.com>
2022-04-14 18:44:08 +0100

    register validation
    prevent empty email, username or password fields from being submitted

--- server/authentication/routes/register.js
+++ server/authentication/routes/register.js
@@ -7,11 +7,16 @@ router.post('/', async (req, res) => {
 
     // Check if email already exists
     const emailExists = await User.findOne({ email: req.body.email })
-    if (emailExists) return res.status(200).send({message :'User with email ' + req.body.email + ' already exists'})
+    if (emailExists) return res.status(200).send({ message: 'User with email ' + req.body.email + ' already exists' })
 
     // hash password
     const salt = await bcrypt.genSalt(10)
+    if (req.body.password == "") return res.status(200).send({ message: 'Password must not be blank' })
+    
     hashed_password = await bcrypt.hash(req.body.password, salt)
+         
+    
+    
 
     // Create a new user to add to the DB
     const user = new User({
@@ -22,11 +27,19 @@ router.post('/', async (req, res) => {
 
     // Check for any errors 
     try {
-        const new_user = await user.save()
-        console.log(new_user.email + ' added successfully')
-        res.send({user : new_user, message: 'user added'})
-
+         if (user.email === "") {
+            res.status(200).send({ message: 'Email must not be blank' })
+        } 
+        else if (user.name === "") {
+            res.status(200).send({ message: 'Username must not be blank' })
+        }
+        else { 
+            const new_user = await user.save()
+            console.log(new_user.email + ' added successfully')
+            res.send({ user: new_user, message: 'user added' })
+        }
     } catch (e) {
+        res.status(200).send({ message: 'Email, username or password must not be blank' })
         console.log("CANNOT ADD USER")
         console.log(e)
     }
