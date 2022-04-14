99d13b64ae5f27c8e32312d491349c37416d9924
shaiban <sl01347@surrey.ac.uk>
2022-04-14 01:09:41 +0100

    handle creatpost societies

--- front-end/src/components/CreatePost/CreatePost.js
+++ front-end/src/components/CreatePost/CreatePost.js
@@ -3,28 +3,24 @@ import Axios from "axios";
 import { useNavigate } from "react-router-dom";
 import React, { useContext, useEffect } from "react";
 import {
-    Form,
-    FormGroup,
-    Button,
-    Container,
-    Row,
-    Col,
-    Card,
-    Dropdown,
-    Alert
+  Form,
+  FormGroup,
+  Button,
+  Container,
+  Row,
+  Col,
+  Card,
+  Dropdown,
+  Alert,
 } from "react-bootstrap";
-
-
-
+// TODO: get rid of homepage, replace with generalBoard
 const user_values = JSON.parse(localStorage.getItem("user"));
-//console.log(user_values._id)
 
 function CreatePost() {
-<<<<<<< HEAD
   const [postTitle, setTitle] = useState("");
   const [postContent, setContent] = useState("");
-  const [Society, getSociety] = useState([]);
-  const [postSociety, setSociety] = useState("");
+  const [societies, setSocieties] = useState([]);
+  const [postSociety, setPostSociety] = useState("");
   const [postEvent, setEvent] = useState(Boolean);
   const [error, setError] = useState();
   const [show, setShow] = useState(false);
@@ -58,27 +54,13 @@ function CreatePost() {
     }
   };
   useEffect(() => {
-    
-      // console.log(user_id)
-      // Axios.get('http://localhost:3007/society/mysocieties', {
-      //   params: {
-      //     user_id: "6257075ceea7b1a32c170006"
-      //   }
-      // })
-      // .then(function(res) {
-      //   console.log(res);
-      // })
-
-      Axios.post('http://localhost:3007/society/mysocieties', {
-        
-          user_id: "6257075ceea7b1a32c170006"
-        
-      })
-      .then(function (response) {
-        console.log(response);
-      })
-      
-
+    console.log(user_id);
+    Axios.post("http://localhost:3007/society/mysocieties", {
+      user_id,
+    }).then((response) => {
+      setSocieties(response.data.result[0].society);
+      console.log(response.data.result[0].society)
+    });
   }, []);
 
   return (
@@ -136,16 +118,12 @@ function CreatePost() {
                     id="society"
                     onChange={(e) => {
                       e.preventDefault();
-                      setSociety(e.target.value);
+                      setSocieties(e.target.value);
                     }}
                   >
-                    {Society.map((value, key) => {
-                      return (
-                        <option key={key} value={value._id}>
-                          {value.name}
-                        </option>
-                      );
-                    })}
+                    {/* {societies.map((r) => {
+                      <option key={r[0]._id} value={r[0].society[0]}></option>;
+                    })} */}
                     <option>Select</option>
                   </Form.Select>
                 </div>
@@ -177,142 +155,6 @@ function CreatePost() {
       </Form>
     </Container>
   );
-=======
-    const [postTitle, setTitle] = useState("");
-    const [postContent, setContent] = useState("");
-    const [Society, getSociety] = useState([]);
-    const [postSociety, setSociety] = useState("");
-    const [postEvent, setEvent] = useState(Boolean);
-    const [error, setError] = useState();
-    const [show, setShow] = useState(false);
-    const user_id = user_values._id;
-    const navigate = useNavigate();
-    //const {state, dispatch} = useContext(LoginContext)
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        setShow(false);
-        const post = {
-            title: postTitle,
-            content: postContent,
-            society: postSociety,
-            isEvent: postEvent,
-            user: user_id
-
-        };
-        const res = await Axios.post("http://localhost:3006/post", post);
-        console.log(res.data.message)
-        console.log(post.society)
-        if (res.data.message == 'post added') {
-            navigate("/homepage");
-        } else {
-            setError(res.data.message);
-            setShow(true);
-        }
-
-    };
-    useEffect(() => {
-        Axios.get("http://localhost:3007/society/showall").then((data) => {
-            getSociety(data.data)
-            console.log(data.data)
-        });
-
-    }, []);
-
-    return (
-        
-        <Container fluid="lg">
-            <br />
-            <Form onSubmit={handleSubmit}>
-                <h1 className="center-text">New Post</h1>
-                {show && (
-                    <Alert onClose={() => setShow(false)} variant="danger" dismissible>
-                        <Alert.Heading>{error}</Alert.Heading>
-                    </Alert>
-                )}
-                <Card className="card-padding">
-                    <Row>
-                        <Card.Body>
-                            <Form.Group className="mb-3" controlId="formTitle">
-                                <Form.Label>Title</Form.Label>
-                                <Form.Control
-                                    type="title"
-                                    placeholder="Enter title"
-                                    onChange={(e) => {
-                                        e.preventDefault();
-                                        setTitle(e.target.value);
-                                    }}
-                                />
-                            </Form.Group>
-                        </Card.Body>
-                    </Row>
-                    <Row>
-                        <Card.Body>
-                            <Form.Group className="mb-3" controlId="formDescription">
-                                <Form.Label>Description</Form.Label>
-                                <div className="form-group">
-
-                                    <textarea className="form-control" type="text" placeholder="Enter description" id="description" rows="4" onChange={(e) => {
-                                        e.preventDefault();
-                                        setContent(e.target.value);
-                                    }}
-                                    />
-                                </div>
-                            </Form.Group>
-                        </Card.Body>
-                    </Row>
-                    <Row>
-                        <Card.Body>
-                            <Form.Group className="mb-3" controlId="formSociety">
-                                <Form.Label>Society</Form.Label>
-                                <div className="form-group">
-                                    <select id='society' onChange={(e) => {
-                                        e.preventDefault();
-                                        setSociety(e.target.value);
-
-                                    }}>
-                                        {Society.map((value, key) => {
-                                            return (
-
-                                                <option key={key} value={value._id} >{value.name}</option>
-                                            )
-                                        })}
-                                        <option >Select</option>
-                                    </select>
-                                </div>
-                            </Form.Group>
-                        </Card.Body>
-                    </Row>
-                    <Row>
-                        <Card.Body>
-                            <Form.Group className="mb-3" controlId="formEvent">
-                                <Form.Label>Check the box below if the post is about an event:</Form.Label>
-                                <div className="form-group">
-                                    <input
-                                        type="checkbox"
-                                        onChange={(e) => {
-                                            e.preventDefault();
-                                            setEvent(e.target.checked);
-
-                                        }}
-                                    />
-                                </div>
-                            </Form.Group>
-                        </Card.Body>
-                    </Row>
-                    <div id="align-center">
-                        <Button type="submit" size="lg">
-                            Submit
-                        </Button>
-
-                    </div>
-                    <br />
-
-                </Card>
-            </Form>
-        </Container>
-    );
->>>>>>> 3b455066d635da1f51683219ec2883c056bfd2e2
 }
 
-export default CreatePost
+export default CreatePost;
