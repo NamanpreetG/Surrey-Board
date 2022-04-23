935e6eb487f75f7bff92405e1f99e3e69fea0655
Namanpreet Ghotra <ng00451@surrey.ac.uk>
2022-04-23 15:44:55 +0100

    Connecting database with local references

--- front-end/src/components/EventsBoard/EventsBoard.js
+++ front-end/src/components/EventsBoard/EventsBoard.js
@@ -11,3 +11,59 @@ async function fetchPosts(countPage, page, index) {
   return res.json();
 }
 
+function EventsBoard() {
+  const user = JSON.parse(localStorage.getItem("user"));
+
+  const [countPage, setCountPage] = useState("");
+  const [page, setPage] = useState(1);
+  const [index, setIndex] = useState(0);
+
+  const { isLoading, data, isError, error } = useQuery(
+    ["posts", countPage, page, index],
+    () => fetchPosts(countPage, page, index),
+    {
+      keepPreviousData: true,
+    }
+  );
+
+  if (isError) {
+    return <h2>{error.message}</h2>;
+  }
+
+  const previousPage = () => {
+    setCountPage("previous");
+    setPage((old) => Math.max(old - 1, 1));
+    setIndex(data.result[0].counter);
+  };
+
+  const nextPage = () => {
+    setCountPage("next");
+    setPage((old) => (!data.result || data.next === 0 ? old : old + 1));
+    setIndex(data.result.at(-1).counter);
+  };
+
+  return (
+    // TODO: add tag to SinglePost
+    <>
+      {isLoading ? (
+        <div>Fetching data...</div>
+      ) : (
+        <>
+          <div>
+            <br />
+            <h1 id="title">Events Board</h1>
+            <br />
+            {data.result &&
+              data.result.map((r) => (
+                <SinglePost
+                  key={r._id}
+                  title={r.title}
+                  description={r.content}
+                  date={r.date}
+                  likes={r.likes}
+                  username={r.user ? r.user.name : ""}
+                  id={r._id}
+                  tag={r.society.tag}
+                />
+              ))}
+         
