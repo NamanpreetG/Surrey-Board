5c86bcc9b7e5e743ad3912f014e76b66ce67788e
Namanpreet Ghotra <ng00451@surrey.ac.uk>
2022-04-23 15:43:14 +0100

    User state logged in

---
+++ front-end/src/components/EventsBoard/EventsBoard.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { useContext, useEffect, useState } from "react";
+import { LoginContext, loginContext } from "../../App";
+import SinglePost from "../Posts/SinglePost";
+import { useQuery } from "react-query";
+import { Button } from "react-bootstrap";
+
+async function fetchPosts(countPage, page, index) {
+  let url = `http://localhost:3006/showpost/events/${countPage}?page=${page}&index=${index}`;
+  const res = await fetch(url);
+  return res.json();
+}
+
+function EventsBoard() {
+  const user = JSON.parse(localStorage.getItem("user"));
+
+  
